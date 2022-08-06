c8443f8caadb55754e504e0c91b4b25cd33e995e
Hristo Sandev <hristosandev@Hristos-MacBook-Air.local>
2022-08-06 18:31:17 -0400

    Add navigation tabs

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+//tabs import
+import Home from './tabs/Home.js'
+import Settings from './tabs/Settings'
+
+Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home}/>
+        <Tab.Screen name='Settings' component={Settings}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
