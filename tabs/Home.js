d2753055b9a16bd7a9a438181bc20693310adb3d
Hristo Sandev <hristosandev@Hristos-MacBook-Air.local>
2022-08-07 01:02:03 -0400

    add exercise list

--- tabs/Home.js
+++ tabs/Home.js
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput, ScrollView, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+
+    const [textS, setTextS] = useState('')
+    const [exercises, setExercises] = useState([]) //currently for testing, this will be stored locally or on a server later
+
+    const handleCreate = () => {
+        if (textS === '') {return;}
+        setExercises((prevExercises) => {
+            return [...prevExercises, {name:textS, id: Math.floor(Math.random() * 10000000000000).toString()}]
+        })
+        setTextS('')
+    }
+
+    useEffect(() => {
+        console.log('The exercises are: ', exercises)
+    }, [exercises])
+
   return (
     <View>
-      <Text>Home</Text>
+        <Text>Exercise logs:</Text>
+        <TextInput 
+            style={{
+                height: 40,
+                margin: 12,
+                borderWidth: 1,
+                padding: 10,
+              }}
+            placeHolder='Enter exercise name'
+            onChangeText={setTextS}
+            value={textS}
+        />
+        <Button
+            title='Add exercise'
+            onPress={handleCreate}
+        />  
+        <ScrollView>
+            <FlatList
+              data={exercises}
+              renderItem={({item}) => (
+                <View>
+                    <Text> {item.name} </Text>
+                </View>
+              )}
+              keyExtractor={item => item.id}
+            />
+        </ScrollView>
     </View>
   )
 }
