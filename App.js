cf2562a2dd6e2564adbeda070ec47a365ac9fd82
Hristo Sandev <hristosandev@Hristos-MacBook-Pro.local>
2022-08-19 21:32:04 -0400

    Add icons to tabs

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Provider as PaperProvider, MD3LightTheme as DefaultTheme} from 'react-native-paper';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  Provider as PaperProvider,
+  MD3LightTheme as DefaultTheme,
+} from "react-native-paper";
 //tabs import
-import HomeStackScreen from './tabs/HomeStackScreen.js'
-import Settings from './tabs/Settings'
-
+import HomeStackScreen from "./tabs/HomeStackScreen.js";
+import Settings from "./tabs/Settings";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 Tab = createBottomTabNavigator();
 
@@ -15,15 +19,31 @@ export default function App() {
     <PaperProvider theme={DefaultTheme}>
       <NavigationContainer>
         <Tab.Navigator
-        sceneContainerStyle={{ backgroundColor: 'white' }}
-        >
-          <Tab.Screen name='Workout' component={HomeStackScreen}/>
-          <Tab.Screen name='Settings' component={Settings}/>
-        </Tab.Navigator>
-        
+          sceneContainerStyle={{ backgroundColor: "white" }}
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
+              if (route.name === "Workout") {
+                iconName = "weight-lifter"
+                return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+
+              } else if (route.name === "Settings") {
+                iconName = "settings"
+                return <Ionicons name={iconName} size={size} color={color} />
+
+              }
+
+              // You can return any component that you like here!
+            },
+            tabBarActiveTintColor: "green",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen name="Workout" component={HomeStackScreen} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
       </NavigationContainer>
     </PaperProvider>
   );
 }
-
